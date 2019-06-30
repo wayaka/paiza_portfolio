ef7c9eccec4768273243e1900476ab704781d8d9
渡邉彩花 <ayaka@watanabeayakanoMacBook-puro.local>
2019-06-30 20:11:34 +0900

    install devise gem

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
@@ -61,6 +61,10 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+# install bootstrap
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem 'mini_racer'
+
+# install devise gem for login
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   root  'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :codes, except: :show
+
+  devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'   
+} 
+
+devise_scope :user do
+  get "user/:id", :to => "users/registrations#detail"
+  get "signup", :to => "users/registrations#new"
+  get "login", :to => "users/sessions#new"
+  get "logout", :to => "users/sessions#destroy"
+end
 end
