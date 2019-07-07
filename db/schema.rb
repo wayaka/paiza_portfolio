a1fd85ee804f11d263abd5294fc29a2e0b0bae51
渡邉彩花 <ayaka@watanabeayakanoMacBook-puro.local>
2019-07-07 11:11:36 +0900

    create languages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_01_120937) do
+ActiveRecord::Schema.define(version: 2019_07_07_021018) do
+
+  create_table "languages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
