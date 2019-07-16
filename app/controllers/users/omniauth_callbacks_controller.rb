535bd719b4bbd6afb44a800c529e756d3f858d0e
渡邉彩花 <ayaka@watanabeayakanoMacBook-puro.local>
2019-07-16 23:19:33 +0900

    signup with twitter

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,12 +12,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # common callback method
   def callback_for(provider)
+    puts request.env["omniauth.auth"] 
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      byebug
       redirect_to new_user_registration_url
     end
   end
